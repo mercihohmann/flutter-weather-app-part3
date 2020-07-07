79ba1f75eeb07cf376640571832ae4a2f0169d50
mercihohmann <64263104+mercihohmann@users.noreply.github.com>
2020-07-07 17:55:12 +0200

    Update main.dart

--- weather_app_part3/main.dart
+++ weather_app_part3/main.dart
@@ -15,7 +15,7 @@ class _WeatherAppState extends State<WeatherApp> {
   String location = 'San Francisco';
   int woeid = 2487956;
   String weather = 'clear';
-  String abbrevation = '';
+  String abbreviation = '';
   String errorMessage = '';
 
   String searchApiUrl =
